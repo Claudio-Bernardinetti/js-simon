db0946d0109ba7d439c295782142de440a991484
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-09-05 14:34:55 +0100

    add countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,26 @@ Esistono dei metodi per trasformare una data in millisecondi?
 
  */
 
-console.log('ciao');
+
+
+const targetDate = new Date('2023-09-06T08:30:00'); // Set the target date and time here
+const countdownElement = document.querySelector('.countdown'); // Select the element where the countdown will be displayed
+
+const updateCountdown = () => {
+    const now = new Date();
+    const timeRemaining = targetDate - now;
+    
+    if (timeRemaining > 0) {
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+        
+        countdownElement.textContent = `${days} Days - ${hours} Hours - ${minutes} Minutes - ${seconds} Seconds remaining`;
+    } else {
+        countdownElement.textContent = 'The time has come!';
+        clearInterval(countdownInterval);
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
